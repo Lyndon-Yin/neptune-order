85d0ceb449f8f3f2f142b72af42e0494bf71a4a7
殷志军 <2353171512@qq.com>
2020-11-06 16:37:14 +0800

    dev环境增加sql打印日志

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+
+use Lyndon\Logger\Log;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +15,19 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        // 本地开发环境打印sql日志
+        if (app()->environment('dev')) {
+            \DB::listen(function ($query) {
+                $i = 0;
+                $rawSql = preg_replace_callback('/\?/', function ($matches) use ($query, &$i) {
+                    $item = isset($query->bindings[$i]) ? $query->bindings[$i] : $matches[0];
+                    $i++;
+                    return gettype($item) == 'string' ? "'$item'" : $item;
+                }, $query->sql);
+
+                Log::filename('sql')->info('sql(' . $query->time . ')', $rawSql);
+            });
+        }
     }
 
     /**
