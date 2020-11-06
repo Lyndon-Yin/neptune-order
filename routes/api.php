60fd253927a3d58c31135882d76970a014832689
殷志军 <2353171512@qq.com>
2020-11-06 16:36:42 +0800

    初始化项目路由

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group([
+    'middleware' => [
+    ]
+], function ($router) {
+    $router->any('{slug?}', function (Request $request) {
+        return \Lyndon\Route\Path4Router::route($request);
+    })->where('slug', '(.*)?');
 });
